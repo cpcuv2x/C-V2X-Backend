5ae2dfd27474154668ef463769fbb3d7a0fbff27
Chinnapat.N <kharkkr@gmail.com>
2023-09-23 12:27:45 +0700

    Implement camera model

---
+++ models/Camera.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const CameraSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please add a name'],
+	},
+	position: {
+		type: 'Front' | 'Back',
+		required: [true, 'Please add a position'],
+	},
+	car_id: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'Car',
+		required: [true, 'Please add a name'],
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = mongoose.model('Camera', CameraSchema);
