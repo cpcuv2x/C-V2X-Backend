76e43ee3b0e9a8645cf08565e932b2541b8df4b5
Chinnapat.N <kharkkr@gmail.com>
2023-09-23 12:28:10 +0700

    Implement rsu model

---
+++ models/RSU.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const RSUSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please add a name'],
+	},
+	recommended_speed: {
+		type: Number,
+		required: [true, 'Please add a recommended speed'],
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = mongoose.model('RSU', RSUSchema);
