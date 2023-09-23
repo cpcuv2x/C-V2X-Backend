38c30f0892aa0a5eebfa1f42a93b3c1fcc8b58fc
Chinnapat.N <kharkkr@gmail.com>
2023-09-23 13:08:28 +0700

    Connect db and routers

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
-const app=express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => {
-    //1. res.send('<h1>Hello from express</h1>');
-    //2. res.send({name:'Brad'});
-    //3. res.json({name:'Brad'});
-    //4. res.sendStatus(400);
-    //5. res.status(400).json({success:false});
-    res.status(200).json({success:true, data:{id:1}});
-    });
-    const PORT=5000;
-    app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+connectDB();
+
+// Route files
+const cars = require('./routes/cars');
+const cameras = require('./routes/cameras');
+const drivers = require('./routes/cars');
+const rsus = require('./routes/rsus');
+
+const app = express();
+
+//Body parser
+app.use(express.json());
+app.use('/api/cars', cars);
+app.use('/api/cameras', cameras);
+app.use('/api/drivers', drivers);
+app.use('/api/rsus', rsus);
+
+const server = app.listen(
+	PORT,
+	console.log(
+		'Server running in ',
+		process.env.NODE_ENV,
+		' mode on port ',
+		PORT
+	)
+);
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`);
+	//Close server & exit process
+	server.close(() => process.exit(1));
+});
