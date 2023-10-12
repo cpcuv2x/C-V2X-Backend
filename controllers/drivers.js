139486de590106864793b25d807e58e7d6be588e
Chinnapat.N <kharkkr@gmail.com>
2023-10-12 22:21:46 +0700

    Optimize driver query

--- controllers/drivers.js
+++ controllers/drivers.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Driver = require('../models/Driver');
 
 //@desc     Get all drivers
@@ -5,20 +6,21 @@ const Driver = require('../models/Driver');
 //@access   Public
 exports.getDrivers = async (req, res, next) => {
 	try {
-		const drivers = await Driver.find();
-		const mapped_drivers = drivers.map((driver) => {
-			return {
-				id: driver._id,
-				name: driver.first_name + ' ' + driver.last_name,
-				first_name: driver.first_name,
-				last_name: driver.last_name,
-				phone_no: driver.phone_no,
-				username: driver.username,
-			};
-		});
+		const drivers = await Driver.aggregate([
+			{
+				$project: {
+					id: '$_id',
+					name: { $concat: ['$first_name', ' ', '$last_name'] },
+					first_name: 1,
+					last_name: 1,
+					phone_no: 1,
+					username: 1,
+				},
+			},
+		]);
 		res
 			.status(200)
-			.json({ success: true, count: drivers.length, data: mapped_drivers });
+			.json({ success: true, count: drivers.length, data: drivers });
 	} catch (err) {
 		res.status(400).json({ success: false });
 	}
@@ -29,16 +31,17 @@ exports.getDrivers = async (req, res, next) => {
 //@access   Public
 exports.getDriversList = async (req, res, next) => {
 	try {
-		const drivers = await Driver.find();
-		const mapped_drivers = drivers.map((driver) => {
-			return {
-				id: driver._id,
-				name: driver.first_name + ' ' + driver.last_name,
-			};
-		});
+		const drivers = await Driver.aggregate([
+			{
+				$project: {
+					id: '$_id',
+					name: { $concat: ['$first_name', ' ', '$last_name'] },
+				},
+			},
+		]);
 		res
 			.status(200)
-			.json({ success: true, count: drivers.length, data: mapped_drivers });
+			.json({ success: true, count: drivers.length, data: drivers });
 	} catch (err) {
 		res.status(400).json({ success: false });
 	}
@@ -49,20 +52,26 @@ exports.getDriversList = async (req, res, next) => {
 //@access   Public
 exports.getDriver = async (req, res, next) => {
 	try {
-		const driver = await Driver.findById(req.params.id);
+		const driver = await Driver.aggregate([
+			{
+				$match: { _id: new mongoose.Types.ObjectId(req.params.id) },
+			},
+			{
+				$project: {
+					id: '$_id',
+					name: { $concat: ['$first_name', ' ', '$last_name'] },
+					first_name: 1,
+					last_name: 1,
+					phone_no: 1,
+					username: 1,
+				},
+			},
+		]);
 
 		if (!driver) {
 			res.status(400).json({ success: false });
 		}
-		const mapped_driver = {
-			id: driver._id,
-			name: driver.first_name + ' ' + driver.last_name,
-			first_name: driver.first_name,
-			last_name: driver.last_name,
-			phone_no: driver.phone_no,
-			username: driver.username,
-		};
-		res.status(200).json({ success: true, data: mapped_driver });
+		res.status(200).json({ success: true, data: driver });
 	} catch (err) {
 		res.status(400).json({ success: false });
 	}
