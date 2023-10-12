930fd9eb19cdb5b15959d5af3a7a295d80aa302d
Chinnapat.N <kharkkr@gmail.com>
2023-10-12 11:02:56 +0700

    Add camera's routes

--- routes/cameras.js
+++ routes/cameras.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { getCameras } = require('../controllers/cameras');
+const {
+	getCameras,
+	createCamera,
+	updateCamera,
+	deleteCamera,
+	getCamera,
+} = require('../controllers/cameras');
 
 const router = express.Router();
 
-router.route('/').get(getCameras);
+router.route('/').get(getCameras).post(createCamera);
+router.route('/:id').get(getCamera).put(updateCamera).delete(deleteCamera);
 
 module.exports = router;
