c24b6e2f2998d8fe4a886a05871529dbaf1a99e9
Chinnapat.N <kharkkr@gmail.com>
2023-10-12 11:03:06 +0700

    Add driver's routes

--- routes/drivers.js
+++ routes/drivers.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { getDrivers } = require('../controllers/drivers');
+const {
+	getDrivers,
+	createDriver,
+	updateDriver,
+	deleteDriver,
+	getDriver,
+} = require('../controllers/drivers');
 
 const router = express.Router();
 
-router.route('/').get(getDrivers);
+router.route('/').get(getDrivers).post(createDriver);
+router.route('/:id').get(getDriver).put(updateDriver).delete(deleteDriver);
 
 module.exports = router;
