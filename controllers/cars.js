f9524c3975b81411b83be272017d9def982630d6
Chinnapat.N <kharkkr@gmail.com>
2023-10-13 11:43:17 +0700

    Map cars response

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,11 +1,73 @@
+const mongoose = require('mongoose');
 const Car = require('../models/Car');
+const Driver = require('../models/Driver');
 
 //@desc     Get all cars
-//@route    GET /api/cars
+//@route    PUT /api/cars
 //@access   Public
 exports.getCars = async (req, res, next) => {
 	try {
-		const cars = await Car.find();
+		const cars = await Car.aggregate([
+			{
+				$lookup: {
+					from: 'drivers',
+					localField: 'driver_id',
+					foreignField: '_id',
+					as: 'driverInfo',
+				},
+			},
+			{
+				$lookup: {
+					from: 'cameras',
+					localField: '_id',
+					foreignField: 'car_id',
+					as: 'cameras',
+				},
+			},
+			{
+				$project: {
+					_id: 1,
+					driver_id: 1,
+					driver: {
+						$concat: [
+							{ $arrayElemAt: ['$driverInfo.first_name', 0] },
+							' ',
+							{ $arrayElemAt: ['$driverInfo.last_name', 0] },
+						],
+					},
+					cameras: {
+						$map: {
+							input: '$cameras',
+							as: 'camera',
+							in: {
+								id: '$$camera._id',
+								name: '$$camera.name',
+								position: '$$camera.position',
+							},
+						},
+					},
+				},
+			},
+		]);
+		res.status(200).json({ success: true, count: cars.length, data: cars });
+	} catch (err) {
+		res.status(400).json({ success: false });
+	}
+};
+
+//@desc     Get all cars list
+//@route    GET /api/cars/list
+//@access   Public
+exports.getCarsList = async (req, res, next) => {
+	try {
+		const cars = await Car.aggregate([
+			{
+				$project: {
+					id: '$_id',
+					name: 1,
+				},
+			},
+		]);
 		res.status(200).json({ success: true, count: cars.length, data: cars });
 	} catch (err) {
 		res.status(400).json({ success: false });
@@ -17,7 +79,51 @@ exports.getCars = async (req, res, next) => {
 //@access   Public
 exports.getCar = async (req, res, next) => {
 	try {
-		const car = await Car.findById(req.params.id);
+		const car = await Car.aggregate([
+			{
+				$match: { _id: new mongoose.Types.ObjectId(req.params.id) },
+			},
+			{
+				$lookup: {
+					from: 'drivers',
+					localField: 'driver_id',
+					foreignField: '_id',
+					as: 'driverInfo',
+				},
+			},
+			{
+				$lookup: {
+					from: 'cameras',
+					localField: '_id',
+					foreignField: 'car_id',
+					as: 'cameras',
+				},
+			},
+			{
+				$project: {
+					_id: 1,
+					driver_id: 1,
+					driver: {
+						$concat: [
+							{ $arrayElemAt: ['$driverInfo.first_name', 0] },
+							' ',
+							{ $arrayElemAt: ['$driverInfo.last_name', 0] },
+						],
+					},
+					cameras: {
+						$map: {
+							input: '$cameras',
+							as: 'camera',
+							in: {
+								id: '$$camera._id',
+								name: '$$camera.name',
+								position: '$$camera.position',
+							},
+						},
+					},
+				},
+			},
+		]);
 
 		if (!car) {
 			res.status(400).json({ success: false });
@@ -32,8 +138,24 @@ exports.getCar = async (req, res, next) => {
 //@route    POST /api/cars
 //@access   Public
 exports.createCar = async (req, res, next) => {
-	const car = await Car.create(req.body);
-	res.status(201).json({ success: true, data: car });
+	if (req.body.driver_id && req.body.driver_id.length !== 0) {
+		const driver = await Driver.exists({
+			_id: new mongoose.Types.ObjectId(req.body.driver_id),
+		});
+		if (!driver) {
+			res.status(400).json({ success: false });
+		} else {
+			const car = await Car.create(req.body);
+			res.status(201).json({ success: true, data: car });
+		}
+	} else {
+		const car = await Car.create({
+			name: req.body.name,
+			license_plate: req.body.license_plate,
+			model: req.body.model,
+		});
+		res.status(201).json({ success: true, data: car });
+	}
 };
 
 //@desc     Update car
@@ -41,16 +163,45 @@ exports.createCar = async (req, res, next) => {
 //@access   Public
 exports.updateCar = async (req, res, next) => {
 	try {
-		const car = await Car.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-			runValidators: true,
-		});
+		if (req.body.driver_id && req.body.driver_id.length !== 0) {
+			const driver = await Driver.exists({
+				_id: new mongoose.Types.ObjectId(req.body.driver_id),
+			});
+			if (!driver) {
+				res.status(400).json({ success: false });
+			} else {
+				const car = await Car.findByIdAndUpdate(req.params.id, req.body, {
+					new: true,
+					runValidators: true,
+				});
 
-		if (!car) {
-			res.status(400).json({ success: false });
+				if (!car) {
+					res.status(400).json({ success: false });
+				}
+
+				res.status(200).json({ success: true, data: car });
+			}
+		} else {
+			const car = await Car.findByIdAndUpdate(
+				req.params.id,
+				{
+					name: req.body.name,
+					license_plate: req.body.license_plate,
+					model: req.body.model,
+					driver_id: null,
+				},
+				{
+					new: true,
+					runValidators: true,
+				}
+			);
+
+			if (!car) {
+				res.status(400).json({ success: false });
+			}
+
+			res.status(200).json({ success: true, data: car });
 		}
-
-		res.status(200).json({ success: true, data: car });
 	} catch (err) {
 		res.status(400).json({ success: false });
 	}
--- routes/cars.js
+++ routes/cars.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const {
 	getCars,
+	getCarsList,
+	getCar,
 	createCar,
 	updateCar,
 	deleteCar,
-	getCar,
 } = require('../controllers/cars');
 
 const router = express.Router();
 
-router.route('/').get(getCars).post(createCar);
+router.route('/').put(getCars).post(createCar);
+router.route('/list').get(getCarsList);
 router.route('/:id').get(getCar).put(updateCar).delete(deleteCar);
 
 module.exports = router;
