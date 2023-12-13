3cc26c7ea37bdfea1a3d297603adb3f30e4af7bb
Chinnapat.N <kharkkr@gmail.com>
2023-12-13 17:26:42 +0700

    [ADD] Add emergency regex

--- utils/regex.js
+++ utils/regex.js
@@ -3,3 +3,4 @@ exports.numberRegex = /^[0-9]+$/;
 exports.passwordRegex = /^(?=\S{8,}$).*/;
 exports.phoneNoRegex = /^(\d{3}-\d{3}-\d{4})$/;
 exports.positionRegex = /^(Front|Back)$/;
+exports.emergencyRegex = /^(pending|inProgress|complete)$/;
