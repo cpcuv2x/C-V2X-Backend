b5a1b939c4a78de020e7de74c1a6b751ed1ad1c0
Chinnapat.N <kharkkr@gmail.com>
2023-12-13 10:43:46 +0700

    [UPDATE] Add position regex

--- utils/regex.js
+++ utils/regex.js
@@ -2,3 +2,4 @@ exports.noSpaceRegex = /^\S*$/;
 exports.numberRegex = /^[0-9]+$/;
 exports.passwordRegex = /^(?=\S{8,}$).*/;
 exports.phoneNoRegex = /^(\d{3}-\d{3}-\d{4})$/;
+exports.positionRegex = /^(Front|Back)$/;
