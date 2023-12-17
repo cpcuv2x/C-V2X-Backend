0d5b1620e8818abf1d693ba572abc1cf2ce7fa5c
Chinnapat.N <kharkkr@gmail.com>
2023-12-17 15:01:07 +0700

    [UPDATE] Setup swagger

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const helmet = require('helmet');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
-//Connect to database
+// Connect to database
 connectDB();
 
 const limiter = rateLimit({
@@ -37,17 +39,52 @@ app.use(hpp()); //Prevent http param pollutions
 app.use(mongoSanitize()); //Sanitize data
 app.use(xss()); //Prevent XSS attacks
 
-// Routes
+// Body parser
 app.use('/api/cars', cars);
 app.use('/api/cameras', cameras);
 app.use('/api/drivers', drivers);
 app.use('/api/rsus', rsus);
 app.use('/api/emergencies', emergencies);
 
-//Cookie parser
+// Cookie parser
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
+
+//Swagger API
+const swaggerOptions = {
+	swaggerDefinition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'C-V2X API',
+			version: '1.0.0',
+			description: 'C-V2X API documents',
+		},
+		components: {
+			securitySchemes: {
+				BearerAuth: {
+					type: 'http',
+					scheme: 'bearer',
+					bearerFormat: 'JWT',
+				},
+			},
+		},
+		security: [
+			{
+				BearerAuth: [],
+			},
+		],
+		servers: [
+			{
+				url: `http://localhost:${PORT}`,
+			},
+		],
+	},
+	apis: ['./routes/*.js'],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 const server = app.listen(
 	PORT,
 	console.log(
