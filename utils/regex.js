b259256fc24ea47356344646eaf54bdb870b5998
Chinnapat.N <kharkkr@gmail.com>
2024-01-10 16:11:47 +0700

    [UPDATE] Add left/right cameras

--- utils/regex.js
+++ utils/regex.js
@@ -2,5 +2,5 @@ exports.noSpaceRegex = /^\S*$/;
 exports.numberRegex = /^[0-9]+$/;
 exports.passwordRegex = /^(?=\S{8,}$).*/;
 exports.phoneNoRegex = /^(\d{3}-\d{3}-\d{4})$/;
-exports.positionRegex = /^(Front|Back)$/;
+exports.positionRegex = /^(Front|Back|Left|Right)$/;
 exports.emergencyRegex = /^(pending|inProgress|complete)$/;
